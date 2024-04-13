6f59c8678ec952919cb971648d1e61c96ed6153f
maurojjzz <maurojim123@gmail.com>
2024-04-13 16:33:24 -0300

    Header design and functionality

---
+++ src/Components/About/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolore consequatur, ut quibusdam rerum iste ipsa quam id architecto reprehenderit eveniet non quo nemo, voluptatum perspiciatis, cupiditate deserunt voluptatibus minus porro?
+    </div>
+  )
+}
+
+export default About;
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -3,17 +3,19 @@ import styles from "./layout.module.css";
 import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
+import Home from "../Home";
+import About from "../About";
 
 function Layout() {
   return (
-    <div className={`border border-danger container-fluid d-flex flex-column ${styles.containerLayout}`}>
+    <div className={`container-fluid d-flex flex-column ${styles.containerLayout}`}>
       <Router>
         <Header />
         <div className="border border-primary flex-grow-1">
           <Switch>
-            <Route path="/home" exact component={Header} />
-            <Route path="/about" exact component={Footer} />
-            <Route path="/food/:id" component={Header} />
+            <Route path="/home" exact component={Home} />
+            <Route path="/about" exact component={About} />
+            {/* <Route path="/food/:id" component={Header} /> */}
             <Redirect from="/" to="/home" />
           </Switch>
         </div>
